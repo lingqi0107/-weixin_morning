ac3e82398d53b42c93a8d82f7968a0d22dd52d8f
lingqi <tongtongcookie@163.com>
2022-11-01 15:36:53 +0800

    打印19

--- main.py
+++ main.py
@@ -82,9 +82,9 @@ json = [
      "birthday_right": {"month": 1, "day": 7}, "template_id": "zykjLd1EuijeZtiFOhjGf8zxvMvE22PBRgPZPUwqloI",
      "know": "2022-08-16", "city": "长沙"},
     # 包贝
-    {"user_id": "o1nwI6MTh3AlPIEQWNItZyV8BG6M", "type": 2, "birthday_left": {"month": 1, "day": 7},
-     "birthday_right": {"month": 7, "day": 24}, "template_id": "zykjLd1EuijeZtiFOhjGf8zxvMvE22PBRgPZPUwqloI",
-     "know": "2022-08-16", "city": "湘潭"}
+    # {"user_id": "o1nwI6MTh3AlPIEQWNItZyV8BG6M", "type": 1, "birthday_left": {"month": 1, "day": 7},
+    #  "birthday_right": {"month": 7, "day": 24}, "template_id": "zykjLd1EuijeZtiFOhjGf8zxvMvE22PBRgPZPUwqloI",
+    #  "know": "2022-08-16", "city": "湘潭"}
 ]
 
 client = WeChatClient(app_id, app_secret)
@@ -116,9 +116,7 @@ for x in json:
     data = None
     love = None
     if type == 1:
-        love = "这是我追你的第 " + str(love_day) + " 天 "
-    elif type == 2:
-        love = "这是尝试接受他的第 " + str(love_day) + " 天 "
+        love = "距离第一次去江边已有 " + str(love_day) + " 天 "
 
     data = {"today": {"value": today2}, "city": {"value": city}, "weather": {"value": wea},
             "temperature": {"value": temperature}, "love_days": {"value": love},
